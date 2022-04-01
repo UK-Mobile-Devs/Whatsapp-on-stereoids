5cdbdcdd2e97abf0229e98030b7f224bfcfb0b69
Josh Owen <erial909@hotmail.com>
2022-04-01 15:56:11 +0100

    Unstaged change in HomeFragmentVM

--- WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/home/HomeFragmentVM.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/home/HomeFragmentVM.kt
@@ -2,6 +2,7 @@ package com.example.whatsapp.ui.fragments.home
 
 import com.example.whatsapp.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 interface Inputs {
 
@@ -12,7 +13,7 @@ interface Outputs {
 }
 
 @HiltViewModel
-class HomeFragmentVM : BaseViewModel(), Inputs, Outputs {
+class HomeFragmentVM @Inject constructor(): BaseViewModel(), Inputs, Outputs {
 
     val inputs : Inputs = this
     val outputs : Outputs = this
