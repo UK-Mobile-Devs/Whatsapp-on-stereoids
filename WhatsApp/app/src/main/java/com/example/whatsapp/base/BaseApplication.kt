39cfd0019ac84824255487cd34af1bf329af1ff3
Josh Owen <erial909@hotmail.com>
2022-04-01 12:03:31 +0100

    Implemented-Base-Classes
    Implemented base classes for activities, fragments, view models, base application and initialised Hilt.

---
+++ WhatsApp/app/src/main/java/com/example/whatsapp/base/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.example.whatsapp.base
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+}
---
+++ WhatsApp/app/src/main/java/com/example/whatsapp/base/BaseFragment.kt
@@ -0,0 +1,54 @@
+package com.example.whatsapp.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+abstract class BaseFragment<Binding : ViewBinding> : Fragment() {
+
+    lateinit var binding: Binding
+
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            initArgs(it)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = inflateBinding(inflater)
+        initViews()
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        compositeDisposable.clear()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        observeViewModel()
+    }
+
+    abstract fun inflateBinding(layoutInflater: LayoutInflater): Binding
+
+    abstract fun observeViewModel()
+
+    open fun initArgs(arguments: Bundle) {}
+
+    open fun initViews() {}
+}
+
