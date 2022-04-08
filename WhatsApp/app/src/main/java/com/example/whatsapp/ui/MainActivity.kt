c33193caae02c3ca0d3e05f93720726eb1842710
Ugur Denli <ugur6@hotmail.co.uk>
2022-04-08 10:00:27 +0100

    deleting idea folders & git ignore
    deleting idea folders & git ignore

--- WhatsApp/app/src/main/java/com/example/whatsapp/ui/MainActivity.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ui/MainActivity.kt
@@ -1,15 +1,42 @@
 package com.example.whatsapp.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.whatsapp.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment
+            )
+        )
+        setupActionBarWithNavController(navController)
+        navView.setupWithNavController(navController)
+
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
 }
