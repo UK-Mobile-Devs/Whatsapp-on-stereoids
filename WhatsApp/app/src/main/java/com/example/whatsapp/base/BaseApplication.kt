8cee83a214e6b099bc0e57ad549692c0f8b6bbe5
Ugur Denli <ugur6@hotmail.co.uk>
2022-04-08 09:52:42 +0100

    Deleting misc and gitignore
    Deleting misc and gitignore

--- WhatsApp/app/src/main/java/com/example/whatsapp/base/BaseApplication.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/base/BaseApplication.kt
@@ -1,9 +1,21 @@
 package com.example.whatsapp.base
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            SafetyNetAppCheckProviderFactory.getInstance()
+        )
+    }
 
 }
--- WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/home/HomeFragment.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/home/HomeFragment.kt
@@ -1,13 +1,17 @@
 package com.example.whatsapp.ui.fragments.home
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.whatsapp.R
 import com.example.whatsapp.base.BaseFragment
 import com.example.whatsapp.databinding.FragmentHomeBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -20,6 +24,15 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun inflateBinding(layoutInflater: LayoutInflater): FragmentHomeBinding {
        return FragmentHomeBinding.inflate(layoutInflater)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val myRv = view?.findViewById(R.id.fabbtn) as FloatingActionButton
+
+        myRv.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_contactsFragment)
+        }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
