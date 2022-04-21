01269e96a6608a5c157b108bb9c3663d036d018d
oskarlasota <frezzyysmite@gmail.com>
2022-04-21 18:38:18 +0100

    chore: setting up modules for messenger

--- WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/messenger/MessengerFragmentVM.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/messenger/MessengerFragmentVM.kt
@@ -1,5 +1,6 @@
 package com.example.whatsapp.ui.fragments.messenger
 
+import com.example.firestorerepository.repositories.messenger.MessengerRepository
 import com.example.whatsapp.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -7,6 +8,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MessengerFragmentVM @Inject constructor(
-
+    private val messengerRepo : MessengerRepository
 ) : BaseViewModel() {
 }
