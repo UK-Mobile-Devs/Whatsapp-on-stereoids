abb3f31fb0c9e4ceaf3baeaa6b84b7056974fa7d
oskarlasota <frezzyysmite@gmail.com>
2022-04-22 09:02:56 +0100

    fix: switching key ids to Constants.kt

--- WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/messenger/group/GroupMessengerFragment.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/messenger/group/GroupMessengerFragment.kt
@@ -2,6 +2,7 @@ package com.example.whatsapp.ui.fragments.messenger.group
 
 import android.os.Bundle
 import com.example.whatsapp.ui.fragments.messenger.BaseMessengerFragment
+import com.example.whatsapp.utils.Constants.GROUP_ID
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,12 +11,14 @@ class GroupMessengerFragment : BaseMessengerFragment() {
     private lateinit var groupId: String
 
     override fun initArgs(arguments: Bundle) {
-        groupId = arguments.getString("groupId") ?: throw IllegalArgumentException("Missing group chat id")
+        groupId = arguments.getString(GROUP_ID) ?: throw IllegalArgumentException("Missing group chat id")
     }
 
     override fun observeViewModel() {
         viewModel
     }
+
+
 
 
 }
--- WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/messenger/single/SingleMessengerFragment.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/messenger/single/SingleMessengerFragment.kt
@@ -2,6 +2,7 @@ package com.example.whatsapp.ui.fragments.messenger.single
 
 import android.os.Bundle
 import com.example.whatsapp.ui.fragments.messenger.BaseMessengerFragment
+import com.example.whatsapp.utils.Constants.CONTACT_ID
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,7 +11,7 @@ class SingleMessengerFragment : BaseMessengerFragment() {
     private lateinit var contactId: String
 
     override fun initArgs(arguments: Bundle) {
-        contactId = arguments.getString("contactId") ?: throw IllegalArgumentException("Missing contact user id")
+        contactId = arguments.getString(CONTACT_ID) ?: throw IllegalArgumentException("Missing contact user id")
     }
 
     override fun observeViewModel() {
