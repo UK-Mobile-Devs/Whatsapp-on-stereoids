d8ec611c93aa492e4b5cbfa6161ed33caf3575e8
Ugur Denli <ugur6@hotmail.co.uk>
2022-04-22 19:10:46 +0100

    Added gallery to dp
    gallery button added to settings

--- WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/settings/SettingsFragment.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/settings/SettingsFragment.kt
@@ -1,13 +1,19 @@
 package com.example.whatsapp.ui.fragments.settings
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
+import android.view.LayoutInflater
 import androidx.navigation.NavController
-import com.example.whatsapp.R
+import com.example.whatsapp.base.BaseFragment
+import com.example.whatsapp.databinding.FragmentSettingsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
-class SettingsFragment : Fragment(R.layout.fragment_settings) {
+class SettingsFragment : BaseFragment<FragmentSettingsBinding>() {
+
+
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +21,23 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
         super.onCreate(savedInstanceState)
     }
 
+    override fun initViews() {
+        super.initViews()
+        binding.imageView.setOnClickListener{
+            val i = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.INTERNAL_CONTENT_URI
+            )
+            val ACTIVITY_SELECT_IMAGE = 1234
+            startActivityForResult(i, ACTIVITY_SELECT_IMAGE)
+        }
+    }
 
+    //region BaseFragment Overrides
+    override fun inflateBinding(layoutInflater: LayoutInflater): FragmentSettingsBinding {
+        return FragmentSettingsBinding.inflate(layoutInflater)
+    }
+    override fun observeViewModel() {
+
+    }
 }
