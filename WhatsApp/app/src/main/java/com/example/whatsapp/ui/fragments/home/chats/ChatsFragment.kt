ef5c04d1fcad5beb394ee58fe8e3dd71192890a9
Josh Owen <erial909@hotmail.com>
2022-05-14 01:18:03 +0100

    WIP: Implemented an action bar for selecting chats within a recyclerview

--- WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/home/chats/ChatsFragment.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/home/chats/ChatsFragment.kt
@@ -1,9 +1,12 @@
 package com.example.whatsapp.ui.fragments.home.chats
 
+import android.util.Log
+import android.view.ActionMode
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.selection.SelectionPredicates
 import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.selection.StableIdKeyProvider
@@ -17,7 +20,7 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 @AndroidEntryPoint
-class ChatsFragment : BaseFragment<FragmentChatsBinding>() {
+class ChatsFragment : BaseFragment<FragmentChatsBinding>(), ActionMode.Callback {
 
     //region Variables
     private val viewModel: ChatsFragmentVM by viewModels()
@@ -25,6 +28,8 @@ class ChatsFragment : BaseFragment<FragmentChatsBinding>() {
     private val chatsAdapter: ChatsAdapter = ChatsAdapter()
 
     private var tracker: SelectionTracker<Long>? = null
+
+    var actionMode : ActionMode?= null
 
     //endregion
 
@@ -42,8 +47,6 @@ class ChatsFragment : BaseFragment<FragmentChatsBinding>() {
             adapter = chatsAdapter
         }
 
-        this.findNavController()
-
         tracker = SelectionTracker.Builder(
             CHAT_SELECTION_KEY,
             binding.rvChats,
@@ -53,6 +56,45 @@ class ChatsFragment : BaseFragment<FragmentChatsBinding>() {
         ).withSelectionPredicate(
             SelectionPredicates.createSelectAnything()
         ).build()
+
+
+
+
+
+        tracker?.addObserver(object : SelectionTracker.SelectionObserver<Long>() {
+            override fun onSelectionChanged() {
+                Log.e("Selected: ", "New Selection")
+
+            }
+
+            override fun onSelectionRestored() {
+                Log.e("Selected: ", "Restored")
+            }
+
+            override fun onItemStateChanged(key: Long, selected: Boolean) {
+                super.onItemStateChanged(key, selected)
+                Log.e("Selected: ", "New Selection $key")
+
+
+                val size = tracker?.selection?.size()
+                when {
+                    actionMode == null -> {
+                        actionMode = activity?.startActionMode(this@ChatsFragment)
+                    }
+                    size == 0 -> {
+                        actionMode?.finish()
+                    }
+                    else -> {
+                        actionMode?.title = tracker?.selection?.size().toString()
+                    }
+                }
+            }
+
+            override fun onSelectionRefresh() {
+                super.onSelectionRefresh()
+                Log.e("Selected: ", "Refresh")
+            }
+        })
 
         chatsAdapter.tracker = tracker
 
@@ -88,4 +130,53 @@ class ChatsFragment : BaseFragment<FragmentChatsBinding>() {
     }
     //endregion
 
+    //region ActionMode.Callback
+    override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+        mode.menuInflater.inflate(R.menu.home_chats_context_menu, menu)
+        mode.title = tracker?.selection?.size().toString()
+        return true
+    }
+
+    override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
+        return false
+    }
+
+    override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.pinChats -> {
+                Toast.makeText(requireContext(), "Pinned Clicked", Toast.LENGTH_SHORT).show()
+                mode.finish()
+                true
+            }
+            R.id.deleteChats -> {
+                Toast.makeText(requireContext(), "Delete Clicked", Toast.LENGTH_SHORT).show()
+                mode.finish()
+                true
+            }
+            R.id.muteNotifications -> {
+                Toast.makeText(requireContext(), "Mute Clicked", Toast.LENGTH_SHORT).show()
+                mode.finish()
+                true
+            }
+            R.id.archiveMessages -> {
+                Toast.makeText(requireContext(), "Archived Clicked", Toast.LENGTH_SHORT ).show()
+                mode.finish()
+                true
+            }
+            R.id.chatOptions -> {
+                Toast.makeText(requireContext(), "Chat Clicked", Toast.LENGTH_SHORT).show()
+                mode.finish()
+                true
+            }
+            else -> {
+                mode.finish()
+                false
+            }
+        }
+    }
+
+    override fun onDestroyActionMode(p0: ActionMode?) {
+        actionMode = null
+    }
+    //endregion
 }
