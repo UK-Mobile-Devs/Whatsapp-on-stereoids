0ea45f7aa5e43d9490ef3e6c2e00a55535be1e12
oskarlasota <frezzyysmite@gmail.com>
2022-05-21 08:49:03 +0100

    initializing the recycler view with test data

--- WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/contacts/ContactsFragment.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/contacts/ContactsFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.firestorerepository.datatypes.Contact
 import com.example.whatsapp.base.BaseFragment
 import com.example.whatsapp.databinding.FragmentContactsBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,6 +16,7 @@ class ContactsFragment : BaseFragment<FragmentContactsBinding>() {
 
     private lateinit var navController: NavController
     private val viewModel: ContactsFragmentVM by viewModels()
+    private val contactsAdapter = ContactsAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
@@ -23,7 +26,8 @@ class ContactsFragment : BaseFragment<FragmentContactsBinding>() {
     override fun initViews() {
         super.initViews()
         binding.rvContacts.apply {
-
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = contactsAdapter
         }
     }
 
@@ -31,7 +35,7 @@ class ContactsFragment : BaseFragment<FragmentContactsBinding>() {
         FragmentContactsBinding.inflate(layoutInflater)
 
     override fun observeViewModel() {
-
+        contactsAdapter.submitList(listOf(Contact("1"), Contact("2")))
     }
 
 
