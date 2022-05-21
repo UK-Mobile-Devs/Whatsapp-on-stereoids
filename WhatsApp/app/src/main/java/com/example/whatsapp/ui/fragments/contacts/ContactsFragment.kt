6dd56e1179b1548a415592370faf28b9214b9485
oskarlasota <frezzyysmite@gmail.com>
2022-05-21 08:37:16 +0100

    setting up navigation for ContactsFragment.kt

--- WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/contacts/ContactsFragment.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/contacts/ContactsFragment.kt
@@ -1,19 +1,37 @@
 package com.example.whatsapp.ui.fragments.contacts
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
-import com.example.whatsapp.R
+import com.example.whatsapp.base.BaseFragment
+import com.example.whatsapp.databinding.FragmentContactsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class ContactsFragment : Fragment(R.layout.fragment_contacts) {
+class ContactsFragment : BaseFragment<FragmentContactsBinding>() {
+
     private lateinit var navController: NavController
+    private val viewModel: ContactsFragmentVM by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
         super.onCreate(savedInstanceState)
+    }
+
+    override fun initViews() {
+        super.initViews()
+        binding.rvContacts.apply {
+
+        }
+    }
+
+    override fun inflateBinding(layoutInflater: LayoutInflater): FragmentContactsBinding =
+        FragmentContactsBinding.inflate(layoutInflater)
+
+    override fun observeViewModel() {
+
     }
 
 
