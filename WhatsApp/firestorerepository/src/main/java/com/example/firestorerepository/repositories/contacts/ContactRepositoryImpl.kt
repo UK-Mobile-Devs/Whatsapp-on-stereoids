4c4af9d3adef92df1eca97c872f79ebd5c06055b
oskarlasota <frezzyysmite@gmail.com>
2022-05-21 09:06:32 +0100

    setting up repository with return fake data

---
+++ WhatsApp/firestorerepository/src/main/java/com/example/firestorerepository/repositories/contacts/ContactRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package com.example.firestorerepository.repositories.contacts
+
+import com.example.firestorerepository.datatypes.Contact
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ContactRepositoryImpl @Inject constructor() : ContactRepository {
+
+    override fun getContacts(): Observable<List<Contact>> {
+        return Observable.create<List<Contact>> { emitter ->
+            val data = mutableListOf(
+                Contact("1"),
+                Contact("2")
+            )
+            emitter.onNext(data)
+        }.subscribeOn(Schedulers.io())
+    }
+
+    override fun getContact(): Observable<Contact> {
+        return Observable.create {
+            it.onNext(Contact("3"))
+        }
+    }
+
+}
