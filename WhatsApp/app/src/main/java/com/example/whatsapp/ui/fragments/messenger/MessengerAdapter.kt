1c36af2a7562c43dbb80359019873bbc1277a77c
oskarlasota <frezzyysmite@gmail.com>
2022-06-25 11:08:39 +0100

    updating adapter to support new viewholders

--- WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/messenger/MessengerAdapter.kt
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ui/fragments/messenger/MessengerAdapter.kt
@@ -5,51 +5,62 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.example.firestorerepository.datatypes.Message
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.firestorerepository.models.MessageDTO
+import com.example.firestorerepository.models.MessageType
+import com.example.whatsapp.databinding.ItemIncomingMessageBinding
 import com.example.whatsapp.databinding.ItemMessageBinding
 
 class MessengerAdapter :
-    ListAdapter<Message, MessengerAdapter.MessengerViewHolder>(DiffCallback()) {
+    ListAdapter<MessageDTO, ViewHolder>(DiffCallback()) {
 
     init {
         setHasStableIds(true)
     }
 
     //region ListAdapter Overrides
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessengerViewHolder {
-        val view = ItemMessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MessengerViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return when (viewType) {
+            MessageType.PERSONAL.type -> {
+                val view =
+                    ItemMessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                PersonalMessageViewHolder(view)
+            }
+            MessageType.INCOMING.type -> {
+                val view = ItemIncomingMessageBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                IncomingMessageViewHolder(view)
+            }
+            else -> throw IllegalArgumentException("Incorrect message type")
+        }
     }
 
-    override fun onBindViewHolder(holder: MessengerViewHolder, position: Int) {
-        holder.bind(getItem(position))
+    override fun getItemViewType(position: Int): Int {
+        return getItem(position).messageType.type
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        when (holder.itemViewType) {
+            MessageType.PERSONAL.type -> (holder as PersonalMessageViewHolder).bind(getItem(position))
+            MessageType.INCOMING.type -> (holder as IncomingMessageViewHolder).bind(getItem(position))
+        }
     }
 
     override fun getItemId(position: Int): Long = position.toLong()
 
     //endregion
 
-    //region ViewHolder
-    class MessengerViewHolder(val binding: ItemMessageBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(message: Message) {
-            binding.tvMessage.text = "test message :) "
-            binding.tvTimeReceived.text = "15:30"
-        }
-
-    }
-    //endregion
-
     //region ItemCallback
-    class DiffCallback : DiffUtil.ItemCallback<Message>() {
+    class DiffCallback : DiffUtil.ItemCallback<MessageDTO>() {
 
-        override fun areItemsTheSame(oldItem: Message, newItem: Message): Boolean {
+        override fun areItemsTheSame(oldItem: MessageDTO, newItem: MessageDTO): Boolean {
             return oldItem == newItem
         }
 
-        override fun areContentsTheSame(oldItem: Message, newItem: Message): Boolean {
+        override fun areContentsTheSame(oldItem: MessageDTO, newItem: MessageDTO): Boolean {
             return oldItem.id == newItem.id
         }
 
